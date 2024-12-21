bc10b23a37a5497ed12b4e21f34c72fa89ab7a06
Jiamu Sun <barroit@linux.com>
2024-12-22 02:58:06 +0900

    setup: add lowercase to uppercase mapping
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- .setup.sh
+++ .setup.sh
@@ -2,10 +2,25 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 dest=$1/Assets
+name=$(cat NAME)
 
 if [[ ! $1 || ! -d $dest ]]; then
 	>&2 echo 'SDK tree???'
 	exit 128
 fi
 
-ln -sf $PWD/AutoFish* $dest
+mkdir -p $dest/$name
+
+for file in *.asmdef*; do
+	ln -s $PWD/$file $dest/$name/$name.${file#*.}
+done
+
+for file in *_modio.asset*; do
+	ln -s $PWD/$file $dest/${name}_modio.${file#*.}
+done
+
+for file in *.asset*; do
+	ln -s $PWD/$file $dest/$name.${file#*.}
+done
+
+ln -s $PWD/${name,,}.meta $dest/$name.meta

5d78f27e933d7db9bbb498860fdb2245a7558e96
Jiamu Sun <barroit@linux.com>
2024-12-22 03:55:07 +0900

    setup: fix incorrect file glob in wildcard matching
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- .setup.sh
+++ .setup.sh
@@ -10,17 +10,18 @@ if [[ ! $1 || ! -d $dest ]]; then
 fi
 
 mkdir -p $dest/$name
+__name=${name,,}
 
-for file in *.asmdef*; do
+for file in $__name.asmdef*; do
 	ln -s $PWD/$file $dest/$name/$name.${file#*.}
 done
 
-for file in *_modio.asset*; do
+for file in ${__name}_modio.asset*; do
 	ln -s $PWD/$file $dest/${name}_modio.${file#*.}
 done
 
-for file in *.asset*; do
+for file in $__name.asset*; do
 	ln -s $PWD/$file $dest/$name.${file#*.}
 done
 
-ln -s $PWD/${name,,}.meta $dest/$name.meta
+ln -s $PWD/$__name.meta $dest/$name.meta
